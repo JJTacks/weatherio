4e73bb0f1d7496c8a9e8db653bc795f9c576764c
JJTacks <joel.tackey@gmail.com>
2019-02-26 19:36:55 -0500

    Updates

---
+++ precache-manifest.43b74e36221a7c237d3f17d18c0d3baa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc8220669876325b3a09",
+    "url": "/weatherio/static/css/main.0c8abded.chunk.css"
+  },
+  {
+    "revision": "bc8220669876325b3a09",
+    "url": "/weatherio/static/js/main.bc822066.chunk.js"
+  },
+  {
+    "revision": "f8bc2f8854ea8f50ec9f",
+    "url": "/weatherio/static/js/runtime~main.f8bc2f88.js"
+  },
+  {
+    "revision": "1538147e9a44f555206f",
+    "url": "/weatherio/static/css/2.b8a64e3a.chunk.css"
+  },
+  {
+    "revision": "1538147e9a44f555206f",
+    "url": "/weatherio/static/js/2.1538147e.chunk.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weatherio/static/media/img.b6796c73.jpg"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weatherio/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weatherio/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weatherio/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weatherio/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weatherio/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "9dadd18a7450769f3ee7d73dbfaa7e46",
+    "url": "/weatherio/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/weatherio/precache-manifest.43b74e36221a7c237d3f17d18c0d3baa.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/weatherio/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
